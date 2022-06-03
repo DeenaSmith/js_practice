2904e8c0968f789f4d36d9bd869510caafea3213
Deena Smith <deenasmith1630@gmail.com>
2022-06-02 19:40:06 -0500

    plus minus loop/to-decimal practice

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,7 @@ document.getElementById("first").onclick = (e) => {
 };
 
 
+
 // Self-Destruct Button
 document.getElementById("explosion").onclick = (e) => {
     var i = 5;
@@ -42,4 +43,37 @@ document.getElementById("explosion").onclick = (e) => {
 
     startTimer();
 };
+
+
+
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+var arr = [-3, -2, 0, 1, 5]
+
+function plusMinus(arr) {
+    var n = arr.length;
+    var positive = 0;
+    var negative = 0;
+    var zero = 0
+
+    for (var i=0; i < n; i++) {
+        if (arr[i] > 0) {
+        positive += 1;
+        } else if (arr[i] < 0) {
+            negative += 1;
+        } else {
+            zero += 1;
+        }
+    };
+
+    var pos = positive / n;
+    var neg = negative / n;
+    var zer = zero / n;
+
+    console.log(pos.toFixed(6));
+    console.log(neg.toFixed(6));
+    console.log(zer.toFixed(6));
+};
+
+plusMinus(arr);
 
