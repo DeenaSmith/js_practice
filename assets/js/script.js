4a9dd855fbec23c6374e513168b4e4b8b91a594c
Deena Smith <deenasmith1630@gmail.com>
2022-08-15 16:25:54 -0500

    array practice

--- assets/js/script.js
+++ assets/js/script.js
@@ -280,3 +280,29 @@ const name2 = 'Warren';
 
 logEvent(name2, event2);
 logTime(name2, days2);
+
+
+
+
+// Secret Message with Arrays
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+//console.log(secretMessage.length)
+
+secretMessage.push('to', 'Program')
+//console.log(secretMessage)
+
+secretMessage[7] = 'right'
+//console.log(secretMessage)
+
+secretMessage.shift();
+//console.log(secretMessage);
+
+secretMessage.unshift('Programming');
+//console.log(secretMessage)
+
+secretMessage.splice(6, 5, 'know');
+//console.log(secretMessage)
+
+console.log(secretMessage.join(' '))
